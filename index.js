5b96eba7e72252a3f73f11130f30025589831934
aj <ajasus04@gmail.com>
2024-05-11 15:14:37 +0530

    Add intro file

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import "dotenv/config";
+import { fileURLToPath } from "url";
+import path from "path";
 
 import appRoutes from "./routes/routes.js";
 const app = express();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, "intro.txt");
+
+app.get("/", (req, res) => {
+  res.sendFile(filePath);
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", appRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Resource Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING)
